2e7a675fe942ccbc13c6e62838c9b44dfcf3d421
Tubahe171912 <tubahe171912@fpt.edu.vn>
2024-05-13 19:17:59 +0700

    commit home.jsp and homeservlet

--- SWP391/src/java/controller/HomeServlet.java
+++ SWP391/src/java/controller/HomeServlet.java
@@ -53,7 +53,7 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("home.jsp").forward(request, response);
     } 
 
     /** 

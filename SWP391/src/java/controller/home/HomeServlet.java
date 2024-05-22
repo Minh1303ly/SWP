566fb89c795c9b3662b291675722681921648caf
toilaminh13 <minhbqhe171754@fpt.edu.vn>
2024-05-22 18:53:42 +0700

    Update

--- SWP391/src/java/controller/home/HomeServlet.java
+++ SWP391/src/java/controller/home/HomeServlet.java
@@ -121,6 +121,7 @@ public class HomeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        
     }
 
     /**

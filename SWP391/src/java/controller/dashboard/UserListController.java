807c286a9d44bac37924f587fe3253dcc1cff9d6
nguyenduchieu07 <hieundhe171855@fpt.edu.vn>
2024-06-15 23:41:51 +0700

    Update userlist controller

--- SWP391/src/java/controller/dashboard/UserListController.java
+++ SWP391/src/java/controller/dashboard/UserListController.java
@@ -28,35 +28,35 @@ public class UserListController extends HttpServlet {
         currentUser = new User();
         currentUser.setId(1);
 //        if (currentUser != null) {
-            UserDAO uDAO = new UserDAO();
-            List<Roles> role = uDAO.getAllRoles();
-            List<UserStatus> listStatus = uDAO.getAllStatus();
-            String pageParam = request.getParameter("page");
-            String searchParam = request.getParameter("search");
-            String roleParam = request.getParameter("role");
-            String statusParam = request.getParameter("status");
-            Integer role_id = (roleParam != null && !roleParam.isEmpty()) ? Integer.valueOf(roleParam) : null;
-            Integer status = (statusParam != null && !statusParam.isEmpty()) ? Integer.valueOf(statusParam) : null;
+        UserDAO uDAO = new UserDAO();
+        List<Roles> role = uDAO.getAllRoles();
+        List<UserStatus> listStatus = uDAO.getAllStatus();
+        String pageParam = request.getParameter("page");
+        String searchParam = request.getParameter("search");
+        String roleParam = request.getParameter("role");
+        String statusParam = request.getParameter("status");
+        Integer role_id = (roleParam != null && !roleParam.isEmpty()) ? Integer.valueOf(roleParam) : null;
+        Integer status = (statusParam != null && !statusParam.isEmpty()) ? Integer.valueOf(statusParam) : null;
+        // Default to the first page = 10
+        int page = 1;
+        int pageSize = 10; // Set the desired page size
+        if (pageParam != null && !pageParam.isEmpty()) {
+            page = Integer.parseInt(pageParam);
+        }
 
-            int page = 1; // Default to the first page
-            int pageSize = 5; // Set the desired page size
-            if (pageParam != null && !pageParam.isEmpty()) {
-                page = Integer.parseInt(pageParam);
-            }
+        List<User> subjects = uDAO.getAllUserWithParam(currentUser.getId(), searchParam, status, role_id);
+        List<User> pagingUsers = uDAO.Paging(subjects, page, pageSize);
 
-            List<User> subjects = uDAO.getAllUserWithParam(currentUser.getId(), searchParam, status, role_id);
-            List<User> pagingUsers = uDAO.Paging(subjects, page, pageSize);
-
-            request.setAttribute("users", pagingUsers);
-            System.out.println(pagingUsers.size());
-            request.setAttribute("listRole", role);
-            request.setAttribute("listStatus", listStatus);
-            request.setAttribute("totalPages", subjects.size() % pageSize == 0 ? (subjects.size() / pageSize) : (subjects.size() / pageSize + 1));
-            request.setAttribute("currentPage", page);
-            request.setAttribute("searchParam", searchParam);
-            request.setAttribute("roleParam", roleParam);
-            request.setAttribute("statusParam", statusParam);
-            request.getRequestDispatcher("viewsAdmin/viewUsers.jsp").forward(request, response);
+        request.setAttribute("users", pagingUsers);
+        System.out.println(pagingUsers.size());
+        request.setAttribute("listRole", role);
+        request.setAttribute("listStatus", listStatus);
+        request.setAttribute("totalPages", subjects.size() % pageSize == 0 ? (subjects.size() / pageSize) : (subjects.size() / pageSize + 1));
+        request.setAttribute("currentPage", page);
+        request.setAttribute("searchParam", searchParam);
+        request.setAttribute("roleParam", roleParam);
+        request.setAttribute("statusParam", statusParam);
+        request.getRequestDispatcher("viewsAdmin/viewUsers.jsp").forward(request, response);
 //        }else{
 //            response.sendRedirect("home");
 //        }

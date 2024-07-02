86c6b5c5ad444ecc62f4ca5633f81b5672089603
anhdlhe171785 <anhdlhe171785@fpt.edu.vn>
2024-07-02 15:15:48 +0700

    update dashbroad

--- SWP391/src/java/controller/dashboard/DashboardController.java
+++ SWP391/src/java/controller/dashboard/DashboardController.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  * @author 84355
  */
-@WebServlet(name="DashboardController", urlPatterns={"/dashboard"})
+@WebServlet(name="DashboardController", urlPatterns={"/dashboardMKT"})
 public class DashboardController extends HttpServlet {
    
     @Override
@@ -27,13 +27,6 @@ public class DashboardController extends HttpServlet {
 
     } 
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {

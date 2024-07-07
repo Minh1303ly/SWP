da58ae36a046b0f0895f3978eb9e22de1e714ffb
anhdlhe171785 <anhdlhe171785@fpt.edu.vn>
2024-07-07 11:56:32 +0700

    update oderlisst

--- SWP391/src/java/controller/OrderListServlet.java
+++ SWP391/src/java/controller/OrderListServlet.java
@@ -4,6 +4,7 @@
  */
 package controller;
 
+import dao.OrderDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -11,6 +12,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Order;
+import model.Pagination;
 
 /**
  *
@@ -36,7 +44,7 @@ public class OrderListServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet OrderListServlet</title>");            
+            out.println("<title>Servlet OrderListServlet</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet OrderListServlet at " + request.getContextPath() + "</h1>");
@@ -57,7 +65,76 @@ public class OrderListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
+        HttpSession session = request.getSession();
+        session.setAttribute("pagging", "OrderList");
+        
+        int status = 0;
+        String from = "";
+        String to = "";
+        String id = "";
+        String name = "";
+        String sname = "";
+        if (request.getParameter("status") != null) {
+            session.setAttribute("olstatus", request.getParameter("status"));
+        }
+
+        if (request.getParameter("from") != null) {
+            session.setAttribute("olfrom", request.getParameter("from"));
+        }
+
+        if (request.getParameter("to") != null) {
+            session.setAttribute("olto", request.getParameter("to"));
+        }
+
+        if (request.getParameter("id") != null) {
+            session.setAttribute("olid", request.getParameter("id"));
+        }
+
+        if (request.getParameter("name") != null) {
+            session.setAttribute("olname", request.getParameter("name"));
+        }
+        
+        if (request.getParameter("sname") != null) {
+            session.setAttribute("olsname", request.getParameter("sname"));
+        }
+
+        if (session.getAttribute("olstatus") != null) {
+            status = Integer.parseInt((String) session.getAttribute("olstatus"));
+        }
+        if (session.getAttribute("olfrom") != null) {
+            from = (String) session.getAttribute("olfrom");
+        }
+
+        if (session.getAttribute("olto") != null) {
+            to = (String) session.getAttribute("olto");
+        }
+
+        if (session.getAttribute("olid") != null) {
+            id = (String) session.getAttribute("olid");
+        }
+
+        if (session.getAttribute("olname") != null) {
+            name = (String) session.getAttribute("olname");
+        }
+        
+        if (session.getAttribute("olsname") != null) {
+            sname = (String) session.getAttribute("olsname");
+        }
+        try {
+            OrderDAO odao = new OrderDAO();
+            List<Order> olist = odao.getOrders(status, from, to, sname);
+            olist = odao.filterOrders(id, name, olist);
+            int cp = 1;
+            int perpage = 1;
+            if(request.getParameter("cp")!=null) cp = Integer.parseInt(request.getParameter("cp"));
+            if(request.getParameter("perpage")!=null) perpage = Integer.parseInt(request.getParameter("perpage"));
+            session.setAttribute("page", new Pagination<Order>(olist, perpage, cp));
+            request.setAttribute("cp", cp);
+            request.setAttribute("perpage", perpage);
+            request.setAttribute("olist", olist);
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderListServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
         request.getRequestDispatcher("viewsAdmin/orderList.jsp").forward(request, response);
     }
 

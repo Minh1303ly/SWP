e0ea5bed72d79440c86ec36e21764922a5719ece
anhdlhe171785 <anhdlhe171785@fpt.edu.vn>
2024-07-09 21:04:42 +0700

    update

--- SWP391/src/java/controller/dashboard/OrderDetailServlet.java
+++ SWP391/src/java/controller/dashboard/OrderDetailServlet.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controller;
+package controller.dashboard;
 
+import controller.OrderDetailController;
 import dao.OrderDAO;
 import dao.UserDAO;
 import java.io.IOException;
